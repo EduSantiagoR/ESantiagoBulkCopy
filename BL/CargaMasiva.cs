a864e9951f0f42432fe1ff89a2a1b2c16ab5f383
EduSantiagoR <e.santiago.r05@gmail.com>
2023-11-27 10:25:23 -0600

    Add project files.

---
+++ BL/CargaMasiva.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class CargaMasiva
+    {
+        public static ML.Result ReadExcel(string connectionString)
+        {
+            ML.Result result = new ML.Result();
+            using(OleDbConnection context = new OleDbConnection(connectionString))
+            {
+                string query = "SELECT * FROM [RegistrosBulkCopy]";
+                using(OleDbCommand cmd = new OleDbCommand(query, context))
+                {
+                    OleDbDataAdapter adapter = new OleDbDataAdapter();
+                    adapter.SelectCommand = cmd;
+
+                    DataTable tablaProducto = new DataTable();
+                    adapter.Fill(tablaProducto);
+
+                    if(tablaProducto.Rows.Count > 0 )
+                    {
+                        result.Objects = new List<object>();
+                        foreach (DataRow row in tablaProducto.Rows)
+                        {
+                            ML.Producto producto = new ML.Producto();
+                            producto.Proveedor = new ML.Proveedor();
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ PL/Controllers/CargaMasivaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class CargaMasivaController : Controller
+    {
+        // GET: CargaMasiva
+        public ActionResult CargaMasiva()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CargaMasiva(ML.Result result)
+        {
+            HttpPostedFileBase file = Request.Files["Archivo"];
+            if(file != null)
+            {
+                string rutaArchivo = Path.GetFullPath(file.FileName);
+                BL.CargaMasiva.ReadExcel(rutaArchivo);
+            }
+            return View();
+        }
+    }
+}
